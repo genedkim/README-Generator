02a0060a402cdb6cf21dc22e7344eb8a5b12d3da
Gene <genedanielkim@gmail.com>
2023-01-19 23:31:10 -0800

    Completed the generateMarkdown.js creating an array of badge objects and a renderLicenseBadge function to return a badge w/ link for the passed license name using a switch statement. Made some final edits to get everything functioning properly.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => inquirer.prompt([
@@ -34,33 +34,34 @@ const questions = () => inquirer.prompt([
         type: 'list',
         name: 'license',
         message: 'Please choose a license for this project.',
-        choices: ['Apache 2.0', 'BSD 2-Clause', 'BSD 3-Clause', 'GNU GPL', 'GNU LGPL', 'Eclipse Public License 2.0', 'MIT', 'Mozilla Public License 2.0'],
+        choices: ['Apache 2.0', 'BSD 2-Clause', 'BSD 3-Clause', 'GNU GPL v2', 'GNU GPL v3', 'GNU LGPL v3', 'Eclipse Public License 2.0', 'MIT', 'Mozilla Public License 2.0', 'None'],
     },
-    {
-        type: 'input',
-        name: 'readmeName',
-        message: 'Please create a name for your README file (if left blank the file name will be defaulted to README.md)',
-    }
+    // {
+    //     type: 'input',
+    //     name: 'readmeName',
+    //     message: 'Please create a name for your README file (if left blank the file name will be defaulted to README.md)',
+    // }
 
 ]);
 
 // TODO: Create a function to write README file
-const writeToFile = (fileName, data) => {
-    fs.writeFile(fileName, data);
-}
+// const writeToFile = (fileName, data) => {
+//     fs.writeFile(fileName, data);
+// }
 
 // TODO: Create a function to initialize app
 const init = () => {
     console.log('Welcome to Gene\'s README generator. Please answer the following prompts. Once completed the generator will create your custom README file.');
     questions()
         .then((response) => {
-            if (response.readmeName = '') {
-                response.readmeName = 'README'
-            }
-            // writeToFile(`${response.readmeName}.md`, generateMarkdown(response))
+            // console.log(`${response.readmeName}.md`)
+            // if (response.readmeName = '') {
+            //     response.readmeName = 'README'
+            // }
+            // let readmeNameFull = response.readmeName + '.md'
+            fs.writeFile('EXAMPLE.md', generateMarkdown(response), (err) =>
+            err ? console.error(err) : console.log('Your README file is complete! Check your root folder to take a look.'))
         })
-        .then(() => console.log('Your README file is complete! Check your folder to take a look.'))
-        .catch((err) => console.error(err));
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,88 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+const licenseArray = [
+  {
+    name: 'Apache 2.0',
+    badge: 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+    link: 'https://opensource.org/licenses/Apache-2.0',
+  },
+  {
+    name: 'BSD 2-Clause',
+    badge: 'https://img.shields.io/badge/License-BSD%202--Clause-orange.svg',
+    link: 'https://opensource.org/licenses/BSD-2-Clause',
+  },
+  {
+    name: 'BSD 3-Clause',
+    badge: 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg',
+    link: 'https://opensource.org/licenses/BSD-3-Clause',
+  },
+  {
+    name: 'GNU GPL v2',
+    badge: 'https://img.shields.io/badge/License-GPL%20v2-blue.svg',
+    link: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html',
+  },
+  {
+    name: 'GNU GPL v3',
+    badge: 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/gpl-3.0',
+  },
+  {
+    name: 'GNU LGPL v3',
+    badge: 'https://img.shields.io/badge/License-LGPL%20v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/lgpl-3.0',
+  },
+  {
+    name: 'Eclipse Public License 2.0',
+    badge: 'https://img.shields.io/badge/License-EPL%202.0-red.svg',
+    link: 'https://opensource.org/licenses/EPL-2.0',
+  },
+  {
+    name: 'MIT',
+    badge: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    link: 'https://opensource.org/licenses/MIT',
+  },
+  {
+    name: 'Mozilla Public License 2.0',
+    badge: 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
+    link: 'https://opensource.org/licenses/MPL-2.0',
+  }
+]
+
 function renderLicenseBadge(license) {
-
+  switch (license) {
+    case 'Apache 2.0':
+      response = `[![${licenseArray[0].name}](${licenseArray[0].badge})](${licenseArray[0].link})`;
+      break;
+    case 'BSD 2-Clause':
+      response = `[![${licenseArray[1].name}](${licenseArray[1].badge})](${licenseArray[1].link})`;
+      break;
+    case 'BSD 3-Clause':
+      response = `[![${licenseArray[2].name}](${licenseArray[2].badge})](${licenseArray[2].link})`;
+      break;
+    case 'GNU GPL v2':
+      response = `[![${licenseArray[3].name}](${licenseArray[3].badge})](${licenseArray[3].link})`;
+      break;
+    case 'GNU GPL v3':
+      response = `[![${licenseArray[4].name}](${licenseArray[4].badge})](${licenseArray[4].link})`;
+      break;
+    case 'GNU LGPL v3':
+      response = `[![${licenseArray[5].name}](${licenseArray[5].badge})](${licenseArray[5].link})`;
+      break;
+    case 'Eclipse Public License 2.0':
+      response = `[![${licenseArray[6].name}](${licenseArray[6].badge})](${licenseArray[6].link})`;
+      break;
+    case 'MIT':
+      response = `[![${licenseArray[7].name}](${licenseArray[7].badge})](${licenseArray[7].link})`;
+      break;
+    case 'Mozilla Public License 2.0':
+      response = `[![${licenseArray[8].name}](${licenseArray[8].badge})](${licenseArray[8].link})`;
+      break;
+    case 'None':
+      response = "";
+      break;
+  }
+  return response;
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -35,7 +107,9 @@ function generateMarkdown(data) {
   ${data.credits}
 
   ## License
-  ${data.license}
+  ${renderLicenseBadge(data.license)} 
+  
+  This project is licensed under ${data.license} (click the badge above for further information).
 `;
 }
 
